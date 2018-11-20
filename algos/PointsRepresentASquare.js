0be7969202cc3128009196372b2a2916132deea5
“Ishan <ishan.arora@jungleworks.co>
2018-11-20 14:35:12 +0530

    Square Length on all sides

--- algos/PointsRepresentASquare.js
+++ algos/PointsRepresentASquare.js
@@ -19,9 +19,6 @@ class Point {
 
 }
 
-const point1 = new Point(12, 23);
-const point2 = new Point(12, 11);
-
 
 function calculateSquare(point1, point2, point3, point4) {
 
@@ -29,15 +26,24 @@ function calculateSquare(point1, point2, point3, point4) {
     let d2 = Point.distance(point1, point3);
     let d3 = Point.distance(point1, point4);
 
-    if(d1 === d2 && d1 * 2 === d3 && d2 * 2 === Point.distance(point2, point3)) {
-        
-
+    if (d1 === d2 && d1 * 2 === d3 && d2 * 2 === Point.distance(point2, point3)) {
+        const d = Point.distance(point2, point4);
+        return (d === Point.distance(point2, point4) && d === d1);
     }
 
 
+    if (d2 === d3 && d2 * 2 === d1 && d2 * 2 === Point.distance(point3, point4)) {
+        const d = Point.distance(point2, point4);
+        return (d === Point.distance(point2, point3) && d === d2);
+    }
 
 
+    if (d1 === d3 && d1 * 2 === d2 && d1 * 2 === Point.distance(point2, point4)) {
+        const d = Point.distance(point2, point3);
+        return (d === Point.distance(point3, point4) && d === d1);
+    }
 
 
-
+    return 0;
 }
+
