9a125a0af72b9d007299306910b433f0d025a524
“Ishan <ishan.arora@jungleworks.co>
2018-11-25 10:13:47 +0530

    fixes

--- algos/577_a_codeforces.js
+++ algos/577_a_codeforces.js
@@ -2,15 +2,26 @@
  * Created by ishanarora on 25/11/18.
  */
 
+const stdin = process.openStdin();
+let content = ``;
+stdin.addListener('data', d => {
+  content += d.toString();
+});
+
+stdin.addListener('end', () => {
+  content = content.split(" ");
+ console.log(factors(parseInt(content[0]), parseInt(content[1])));
+});
+
+
+
 function factors(elem1, elem2) {
-    
     let count = 0;
-    for (var i = elem1; i > Math.sqrt(elem1); i--) {
-        if (elem2 % i == 0) {
-            count += 2;
+    for (var i = 1; i <= elem1; i++) {
+        if (elem2 % i == 0 && elem2 / i <= elem1) {
+            count += 1;
         }
     }
     return count;
 }
 
-console.log(factors(5,13));
